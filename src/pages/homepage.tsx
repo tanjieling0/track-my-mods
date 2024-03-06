ff3f267285312763905a3414eaf5b28cc327f9a8
Tan Jie Ling <tanjieling3@gmail.com>
2024-03-06 16:50:31 +0800

    Add accordion and dropdown menu

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 import Divider from "@/components/divider";
+import { SlArrowDown } from "react-icons/sl";
 import ProgressBar from "@/components/progress-bar";
-import { Overpass, Radio_Canada } from '@next/font/google';
+import { Overpass, Radio_Canada, Yaldevi } from '@next/font/google';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 /* Fonts */
 const overpass = Overpass({
@@ -14,11 +29,17 @@ const radioCanada = Radio_Canada({
   weight: ['400', '700'],
 })
 
+const yaldevi = Yaldevi({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+})
+
 /* Colors for texts */
 const grayText: string = '#6A6A6A';
 const orangeText: string = '#FF9100';
 const redText: string = '#FE7752';
 const grayBackground: string = '#ECECEC';
+const darkGrayText: string = '#9F9F9F';
 
 
 export default function Homepage() {
@@ -28,7 +49,7 @@ export default function Homepage() {
        {/* Header */}
       <section className={overpass.className}>
         <header className="flex absolute justify-evenly items-center py-16 w-full"> 
-            <h1 className="flex absolute bottom-0 left-0 px-12 text-8xl overpass-text">
+            <h1 className="flex absolute bottom-0 left-0 px-12 text-8xl overpass-text font-bold">
               <p style={{ color: orangeText }}>Jie Ling's</p>
               <p style={{ color: redText }} className="px-4">Mods</p>
             </h1>
@@ -40,14 +61,87 @@ export default function Homepage() {
         <Divider />
       </div>
 
+      {/* Main */}
+      <section className={`flex flex-col absolute px-10 ${radioCanada.className}`}>
+
+        {/* This Semester */}
+        <div className="flex flex-row justify-self-auto">
+          <p className="relative -top-24 text-5xl font-extrabold">
+            This Semester
+          </p>
+          <div>
+          <DropdownMenu>
+            <DropdownMenuTrigger className={`flex px-28 text-2xl -translate-y-[6rem] translate-x-12 rounded-3xl h-12 ${yaldevi.className}`} style={{ background: `${grayBackground}`, color: darkGrayText}}>
+                <div className="flex self-center -translate-x-20 ">
+                  Current Sem
+                  <SlArrowDown className="self-center translate-x-40"/>
+                </div>
+              </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel className="text-xl">Current Semester</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="text-xl">Year 1 Sem 1</DropdownMenuItem>
+              <DropdownMenuItem className="text-xl">Year 1 Sem 2</DropdownMenuItem>
+              <DropdownMenuItem className="text-xl">Year 2 Sem 1</DropdownMenuItem>
+              <DropdownMenuItem className="text-xl">Year 2 Sem 2</DropdownMenuItem>
+              <DropdownMenuItem className="text-xl">Year 3 Sem 1</DropdownMenuItem>
+              <DropdownMenuItem className="text-xl">Year 3 Sem 2</DropdownMenuItem>
+              <DropdownMenuItem className="text-xl">Year 4 Sem 1</DropdownMenuItem>
+              <DropdownMenuItem className="text-xl">Year 4 Sem 2</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          </div>
+        </div>
+        
+        <section className="py-80">
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>YEAR 1</AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>YEAR 2</AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>YEAR 3</AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>YEAR 4</AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+
+        </section>
+
+      </section>
+
       {/* Requirements */}
       <section className="flex flex-col absolute justify-start right-10 rounded-3xl h-[100rem] w-[50rem] -translate-y-24" style={{ background: `${grayBackground}`}}>
         <div className={radioCanada.className}>
-          <h2 className="text-5xl py-8 px-10">Requirements</h2>
+          <h2 className="text-5xl py-8 px-10 font-extrabold">Requirements</h2>
 
           <ul>
             <li className="text-light font-light px-10 text-2xl">
-              <p>Progress</p>
+              <p className="text-3xl">Progress</p>
 
               <div className="py-4 -translate-x-0">
                 <ProgressBar widthDarkGreen={55} widthLightGreen={75} />
